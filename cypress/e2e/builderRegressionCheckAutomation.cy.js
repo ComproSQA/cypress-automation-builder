c3ea99de29da77b739ffcb1e8d0342b01f373a8d
Ankur Sharma <ankur.sharma@comprotechnologies.com>
2024-06-12 10:52:37 +0530

    Committing code refinements

--- cypress/e2e/builderRegressionCheckAutomation.cy.js
+++ cypress/e2e/builderRegressionCheckAutomation.cy.js
@@ -40,14 +40,18 @@ describe('Builder Regression Test',function(){
         cy.get('.tooltip-wrapper > .btn').click(); //Click on Save button
         
         //Creating xpath for the above created umbrella present on the page
+        /*
         const xpath1 = `//*[contains(@title,"${umbrellaUniqueCode}")]`;
         //Wait for the above created umbrella product to be visible and then click on it
         cy.xpath(xpath1).should('be.visible').then(($result)=>{
             cy.xpath(xpath1).click();  
         })
+        */
+        cy.get(`[title="${umbrellaUniqueCode}"]`).contains(`${umbrellaUniqueCode}`).should('be.visible').then(($result)=>{
+            cy.get(`[title="${umbrellaUniqueCode}"]`).contains(`${umbrellaUniqueCode}`).click();
+        })
         cy.get('.page-title').should('contain.text',umbrellaUniqueCode);
-        cy.get('[data-ui-view=""] > .alert').should('be.visible');
-    
+        cy.get('[data-ui-view=""] > .alert').should('be.visible');    
     })
 
     it('Create a component with a unique code starting from "cqa"', function(){        
@@ -58,8 +62,17 @@ describe('Builder Regression Test',function(){
         cy.get(':nth-child(2) > .collapsed').click(); //Open Products option
         cy.get('#sidebar-products > :nth-child(3) > .ng-binding').click(); //Open Components option
         cy.get('.hidden-sm').click(); //Hide sidebar
+
+        //Click on Practice Extra template
+        cy.get('.template-options-widget[role="option"]').find('.builder-carousel-widget').each(($el,index,$list)=>{
+            var templateText=$el.find('.carousel-header').text();
+            if(templateText.includes("Practice Extra"))
+                {
+                    cy.wrap($el).click()
+                }
+        })
+        //cy.get('.slick-current > .builder-carousel-widget').click(); //Click on Practice Extra template
         
-        cy.get('.slick-current > .builder-carousel-widget').click(); //Click on Practice Extra template
         cy.get('#builder-form-field-id').type(peUniqueCode); //Enter unique component code
         cy.get('#select2-chosen-1').click(); //Open CY Year drop-down
         cy.xpath('//*[@id="select2-result-label-5"][contains(text(),"2018")]').click(); //Select 2018 as CY year from drop-down
